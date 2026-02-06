3578084bfac3c48c1aa8d28893ee343a41e1f92b
teerev <trevor.p.hardcastle@gmail.com>
2026-02-05 19:22:06 -0500

    clean state

--- hello.py
+++
@@ -1,5 +0,0 @@
-def greet() -> str:
-    return "Hello, World!"
-
-def farewell() -> str:
-    return "Goodbye, World!"
--- math_utils.py
+++
@@ -1,8 +0,0 @@
-def add(a, b):
-    """Return the sum of a and b."""
-    return a + b
-
-
-def multiply(a, b):
-    """Return the product of a and b."""
-    return a * b
